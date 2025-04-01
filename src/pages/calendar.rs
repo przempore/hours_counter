36114623ba3b088ef5225baa099c579754b4f1a5
Przemek <przempore@gmail.com>
2025-04-01 23:50:01 +0200

    show previous month days, next month days and refactoring

--- src/pages/calendar.rs
+++ src/pages/calendar.rs
@@ -1,46 +1,74 @@
-use leptos::prelude::*;
-use time::Date;
 use crate::utils::calendar::Calendar;
+use leptos::prelude::*;
 
 #[component]
 pub fn Calendar() -> impl IntoView {
-    let weekdays_names = Calendar::WEEKDAYS_NAMES
-                    .into_iter()
-                    .map(|d| {
-                        view! { <div class="weekday">{d}</div> }
-                    })
-                    .collect::<Vec<_>>();
-    let previous_month_days = (24..=28)
-                    .into_iter()
-                    .map(|day| {
-                        view! { <div class="day previous-month">{day.to_string()}</div> }
-                    })
-                    .collect::<Vec<_>>();
-    let today = Calendar::today();
-    let current_month_days = (1..=31)
-                    .into_iter()
-                    .map(|day| {
-                        let mut class_list = String::from("day");
-                        let current_cell_date = Date::from_calendar_date(today.year(), today.month(), day);
-                        if let Ok(cell_date) = current_cell_date {
-                            if cell_date == today {
-                                class_list.push_str(" today");
-                            }
-                        }
-                        if Calendar::is_weekend(today.year(), today.month(), day) {
-                            class_list.push_str(" weekend");
-                        }
-                        view! { <div class=class_list>{day.to_string()}</div> }
-                    })
-                    .collect::<Vec<_>>();
-
     view! {
         <div class="calendar">
-            <h2>"Calendar"</h2>
+            <div class="calendar-header">
+                <span class="month-name">{Calendar::today().month().to_string()}</span>
+                <button class="arrow">" < "</button>
+                <button class="today-button">"Today"</button>
+                <button class="arrow">" > "</button>
+            </div>
 
             <div class="calendar-grid">
-                {weekdays_names} {previous_month_days} {current_month_days}
+                {weekdays_names()} {previous_month_visible_days()} {current_month_days}
+                {next_month_visible_days()}
             </div>
         </div>
     }
+}
+
+fn weekdays_names() -> Vec<impl IntoView> {
+    Calendar::weekdays_names()
+        .into_iter()
+        .map(|d| {
+            view! { <div class="weekday">{d}</div> }
+        })
+        .collect::<Vec<_>>()
+}
+
+fn previous_month_visible_days() -> Vec<impl IntoView> {
+    let current = Calendar::today();
+    let last_month = Calendar::from(current.year(), current.month().previous(), 1);
+    let last_visible = last_month.last_visible_days(&current);
+    (last_visible.0..=last_visible.1)
+        .into_iter()
+        .map(|day| {
+            view! { <div class="day previous-month">{day.to_string()}</div> }
+        })
+        .collect::<Vec<_>>()
+}
+
+fn current_month_days() -> Vec<impl IntoView> {
+    let current = Calendar::today();
+    (1..=current.month().length(current.year()))
+        .into_iter()
+        .map(|day| {
+            let mut class_list = String::from("day");
+            if day == current.date.day() {
+                class_list.push_str(" today");
+            }
+            if Calendar::is_weekend(current.year(), current.month(), day) {
+                class_list.push_str(" weekend");
+            }
+            view! { <div class=class_list>{day.to_string()}</div> }
+        })
+        .collect::<Vec<_>>()
+}
+
+fn next_month_visible_days() -> Vec<impl IntoView> {
+    let current = Calendar::today();
+    let next_month = Calendar::from(current.year(), current.month().next(), 1);
+    (1..=next_month.date.weekday().number_days_from_monday() + 1)
+        .into_iter()
+        .map(|day| {
+            let mut class_list = String::from("day previous-month");
+            if Calendar::is_weekend(current.year(), current.month(), day) {
+                class_list.push_str(" weekend");
+            }
+            view! { <div class=class_list>{day.to_string()}</div> }
+        })
+        .collect::<Vec<_>>()
 }
--- src/utils/calendar.rs
+++ src/utils/calendar.rs
@@ -1,26 +1,59 @@
-use time::{Date, OffsetDateTime, Month, Weekday};
+use time::{Date, Month, OffsetDateTime, Weekday};
 
+#[derive(Debug, Clone)]
 pub struct Calendar {
-
+    pub date: Date,
 }
 
 impl Calendar {
-    pub const WEEKDAYS_NAMES: [&str; 7] = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    pub fn from(year: i32, month: Month, day: u8) -> Self {
+        Self {
+            date: Date::from_calendar_date(year, month, day).expect("correct date format"),
+        }
+    }
 
-    pub const MONTHS_NAMES: [&str; 12] = [
-        "January", "February", "March", "April", "May", "June", "July", "August", "September",
-        "October", "November", "December",
-    ];
+    pub fn today() -> Self {
+        Self {
+            date: OffsetDateTime::now_utc().date(),
+        }
+    }
 
-    pub fn today() -> Date {
-        OffsetDateTime::now_utc().date()
+    pub fn weekdays_names() -> [String; 7] {
+        [
+            Weekday::Monday.to_string(),
+            Weekday::Tuesday.to_string(),
+            Weekday::Wednesday.to_string(),
+            Weekday::Thursday.to_string(),
+            Weekday::Friday.to_string(),
+            Weekday::Saturday.to_string(),
+            Weekday::Sunday.to_string(),
+        ]
+    }
+
+    pub fn month(&self) -> Month {
+        self.date.month()
+    }
+
+    pub fn weekday(&self) -> Weekday {
+        self.date.weekday()
+    }
+
+    pub fn year(&self) -> i32 {
+        self.date.year()
     }
 
     pub fn is_weekend(year: i32, month: Month, day: u8) -> bool {
-    if let Some(date) = Date::from_calendar_date(year, month, day).ok() {
+        let Some(date) = Date::from_calendar_date(year, month, day).ok() else {
+            return false;
+        };
+
         matches!(date.weekday(), Weekday::Saturday | Weekday::Sunday)
-    } else {
-        false
     }
-}
+
+    pub fn last_visible_days(&self, date: &Self) -> (u8, u8) {
+        let first_visible =
+            self.date.month().length(self.date.year()) - date.weekday().number_from_monday() + 2;
+
+        (first_visible, self.month().length(self.year()))
+    }
 }
